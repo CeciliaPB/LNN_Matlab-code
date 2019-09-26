f5b93a3b2f93d39e2e7c3ec954ff54fce51cc3e7
CeciliaPB <47787950+CeciliaPB@users.noreply.github.com>
2019-09-26 12:55:29 +0200

    Add files via upload

---
+++ Neuron analysis/ACorrelogram.m
@@ -0,0 +1,77 @@
+function [HISTvals,X] = ACorrelogram(n1, varargin)
+
+% This function cauculates the autocorrelation of a neuron neurons.
+%
+% INPUT: 
+%   - n1: .mat file with thw timestamps of the neuron. ex. 'GR1_1.mat'
+%   Varargin
+%   - 'bin': bin size of choice in msec. Default 1.
+%   - 'width': msec on each side of the xcorr plot. Default 250.
+%   - 's': saves the figure in the specified file format (jpg, png,
+%   svg, etc.). See saveas help for more info. Default NOT save.
+%
+% OUTPUT:
+%   - HISTvals: histogram bars of the plot
+%   - X: x dim of the plot, in msec.
+%   - without output arguments, the function plots the xcorr
+% 
+% Examples: 
+% [HISTvals, X] = ACorrelogram(n1, 'bin', 1, 'width', 250, 's', 'jpg');
+% ACorrelogram(n1, 's', 'jpg');
+% 
+% -------------------------------------------------------------------------
+% Cecília Pardo-Bellver, 2019
+% Laboratory of Network Neurophysiology
+% Institute of Experimantal Medicine, Hungary.
+%
+% Uses code from MClust, MEX-file AutoCorr.
+% -------------------------------------------------------------------------
+
+load(n1,'TS');
+TS1 = TS;
+
+% Default params
+bin = 1;
+width = 250;
+s = 0;
+
+% Params, read from varargin
+if nargin
+  for i=1:size(varargin,2)
+    switch varargin{i}
+      case 'bins'
+        bin = varargin{i+1};
+      case 'width'
+        width = varargin{i+1};   
+      case 's'
+        s = 1;
+        type = varargin{i+1};
+      otherwise  
+    end
+  end
+else
+end
+
+
+if isempty(TS1)
+    msgbox('No spikes in the chosen clusters.')
+else
+    [histvals,x] = AutoCorr(TS1, bin, width);
+    bar(x,histvals)
+    xlabel('msec')
+    xlabel(['msec (' num2str(bin) 'msec binsize)']);
+    ylabel('rate');
+    title('Autocorrelation');
+    
+if nargout > 0
+    HISTvals = histvals;
+    X = x;
+else
+end
+
+if s == 1
+    saveas(gcf, genvarname([neuron(1:end-4),'_ACorr']), type);
+elseif s ~= 1
+end 
+
+end
---
+++ Neuron analysis/ISI_hist.m
@@ -0,0 +1,81 @@
+function [ISIh, ISIbins] = ISI_hist(neuron,varargin)
+
+% This function calculates the interspike interval of the timestamps of the
+% neuron.
+% 
+% INPUTS: 
+%   - neuron: .mat file with the timestamps of the neuron. ex. 'GR1_1.mat'
+%   Varargin
+%   - 'bins': bins used. Afects the size of the bars in the histogram.
+%   Default 500.
+%   - 's': saves the figure in the specified file format (jpg, png,
+%   svg, etc.). See saveas help for more info. Default NOT save.
+%
+% OUTPUTS: 
+%   - ISIh: array of histogram of the neuron timestamps. 
+%   - ISIbins: num of bins, in the same units as the timestamps.
+%   - without output arguments, the function plots the ISI.
+% 
+% Examples: 
+% [ISIh, ISIbins] = ISI_hist(neuron,'bins',500,'s','jpg')
+% ISI_hist(neuron,'bins',500) 
+% 
+% -------------------------------------------------------------------------
+% Cecília Pardo-Bellver, 2019
+% Laboratory of Network Neurophysiology
+% Institute of Experimantal Medicine, Hungary.
+%
+% Uses code from MClust, MEX-file ndhist.
+% -------------------------------------------------------------------------
+
+if ~exist('TS','var') == 1
+    load(neuron,'TS');
+end
+
+% Default params
+epsilon = 1e-100;
+bins = 500;
+s = 0;
+
+% Params, read from varargin
+if nargin
+  for i=1:size(varargin,2)
+    switch varargin{i}
+      case 'nBins'
+        bins = varargin{i+1};
+      case 's'
+        s = 1;
+        type = varargin{i+1};
+      otherwise  
+    end
+  end
+else
+end
+
+ISI = diff((TS)/10) + epsilon;
+maxLogISI = max(real(log10(ISI)))+1;
+minLogISI = floor(min(real(log10(ISI))));
+
+isiH = ndhist(log10(ISI)', bins, minLogISI, maxLogISI);
+isiB = logspace(minLogISI,maxLogISI,bins);
+
+% Plot 
+plot(isiB, isiH);
+set(gca, 'XScale', 'log', 'XLim', [10^minLogISI 10^maxLogISI]);
+set(gca, 'YTick', max(isiH));
+hold on
+plot([1 1], get(gca, 'YLim'), 'r:')
+hold off
+
+if nargout > 0
+    ISIh = isiH;
+    ISIbins = isiB;
+else
+end
+
+if s == 1
+    saveas(gcf, genvarname([neuron(1:end-4),'_ISI']), type);
+elseif s ~= 1
+end    
+
+end
