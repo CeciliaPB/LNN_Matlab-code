a56204349cd34c58da7581d2666898a890b7638a
34CS-180808\AnalysisPC <pardo-bellber.cecilia@koki.mta.hu>
2021-04-28 12:29:52 +0200

    Small changes

--- load_data/OEtoDat.m
+++ load_data/OEtoDat.m
@@ -5,32 +5,36 @@ function OEtoDat(varargin)
 % INPUTS: 
 %   Varargin: 
 %   - 'processor': processor number. Default 101.
+%   - 'nChan': Number of channels. Default 32.
 % 
 % EXAMPLES
 % OEtoDat;
-% OEtoDat('processor',101);
+% OEtoDat('processor',101,'nChan',64);
 %
 % -------------------------------------------------------------------------
 % Cecília Pardo-Bellver, 2019
 % Laboratory of Network Neurophysiology
-% Institute of Experimantal Medicine, Hungary.
+% Institute of Experimental Medicine, Hungary.
 % -------------------------------------------------------------------------
 processor = 101;
+nChan     = 32;
 
 if nargin
   for i=1:size(varargin,2)
     switch varargin{i}
       case 'processor'
         processor = varargin{i+1};
+      case 'nChan'
+        nChan =  varargin{i+1};
       otherwise
     end
   end
 else
 end
 
-common_avg = common_avg_1ch(cd, 'processor',processor, 'channel_number', 32);
+common_avg = common_avg_1ch(cd, 'processor',processor, 'channel_number', nChan);
 
-for ii = 1:32
+for ii = 1:nChan
 [data, ts, ~] = load_open_ephys_data([cd '\' num2str(processor) '_CH' num2str(ii) '.continuous']);
 
 data = data - common_avg;
